8e414a4ebdf03ffb966ee55c42e3413738b18df8
MikeyBerger <michaeljudahberger@gmail.com>
2021-03-07 12:27:58 -0500

    Levels Finished CutScenes Started

---
+++ LightInTheDarkV5/Assets/Assets/MyScripts/Dialog.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI TextDisplay;
+    public string[] Sentences;
+    public int Index;
+    public float TypingSpeed;
+
+    IEnumerator Type()
+    {
+        foreach (char letter in Sentences[Index].ToCharArray())
+        {
+            TextDisplay.text += letter;
+            yield return new WaitForSeconds(TypingSpeed);
+        }
+        
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
