4bb2b5d8c606793bdbe532cdcda0301c953d00fb
MikeyBerger <michaeljudahberger@gmail.com>
2021-03-09 13:43:19 -0500

    Minor Adjustments

--- LightInTheDarkV5/Assets/Assets/DeletePlayerPrefsV2.cs
+++ LightInTheDarkV5/Assets/Assets/DeletePlayerPrefsV2.cs
@@ -21,11 +21,18 @@ public class DeletePlayerPrefsV2 : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-
         if (CC.IsPressed && collision.gameObject.tag == "Cursor")
         {
             PlayerPrefs.DeleteAll();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (CC.IsPressed && collision.gameObject.tag == "Cursor")
+        {
+            PlayerPrefs.DeleteAll();
+            //Make a pop-up for Y/N to make sure it's what the player wants
         }
     }
 }
