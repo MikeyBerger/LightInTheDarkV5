4dbc5d1401130eb47a275e23e9c364b32e1ac8ca
MikeyBerger <michaeljudahberger@gmail.com>
2021-04-19 13:33:06 -0400

    Real Final Commit
    That last one was a joke (LOL)

--- LightInTheDarkV5/Assets/Assets/MyScripts/Portal.cs
+++ LightInTheDarkV5/Assets/Assets/MyScripts/Portal.cs
@@ -7,11 +7,12 @@ public class Portal : MonoBehaviour
     public bool IsOrange;
     public float Distance;
     private Transform Destination;
+    private Transform Light;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Light = GameObject.FindGameObjectWithTag("Light").GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -32,6 +33,7 @@ public class Portal : MonoBehaviour
         if (Vector2.Distance(transform.position, collision.transform.position) > Distance)
         {
             collision.transform.position = new Vector2(Destination.position.x, Destination.position.y);
+            Light.transform.position = new Vector2(Destination.position.x, Destination.position.y);
         }
     }
 }
